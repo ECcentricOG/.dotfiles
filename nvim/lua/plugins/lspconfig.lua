6d514f68fda465e555a7f9b5746ccaff6871553a
ECcentricOG <umeshunde003@gmail.com>
2025-04-29 22:10:16 +0530

    small fix

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -36,9 +36,6 @@ return {
             lspconfig.angularls.setup({
                 capabilities = capabilities
             })
-            lspconfig.cssls.setup({
-                capabilities = capabilities
-            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {desc = 'Hover'})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {desc = 'Go to Definition'})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {desc = 'Go to References'})
